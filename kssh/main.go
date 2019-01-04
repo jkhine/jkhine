4e7cd134f7322bfac2ff073d2e255458cef9adf7
Joe Khine <joekhine@gmail.com>
2019-01-03 23:59:47 -0500

    adding kssh

---
+++ kssh/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func getHostKey(host string) (ssh.PublicKey, error) {
+	file, err := os.Open(filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts"))
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var hostKey ssh.PublicKey
+	for scanner.Scan() {
+		fields := strings.Split(scanner.Text(), " ")
+		if len(fields) != 3 {
+			continue
+		}
+		if strings.Contains(fields[0], host) {
+			var err error
+			hostKey, _, _, _, err = ssh.ParseAuthorizedKey(scanner.Bytes())
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("error parsing %q: %v", fields[2], err))
+			}
+			break
+		}
+	}
+
+	if hostKey == nil {
+		return nil, errors.New(fmt.Sprintf("no hostkey for %s", host))
+	}
+	return hostKey, nil
+}
+
+func main() {
+	hostKey, err := getHostKey("192.168.1.55")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sshConfig := &ssh.ClientConfig{
+		User: "homeassistant",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("Xplore1!"),
+		},
+		HostKeyCallback: ssh.FixedHostKey(hostKey),
+	}
+
+	connection, err := ssh.Dial("tcp", "192.168.1.55:22", sshConfig)
+	if err != nil {
+		fmt.Printf("Failed to dial: %s", err)
+	}
+
+	session, err := connection.NewSession()
+	if err != nil {
+		fmt.Printf("Failed to create session: %s", err)
+	}
+
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		session.Close()
+		fmt.Printf("request for pseudo terminal failed: %s", err)
+		log.Fatal()
+	}
+
+	stdin, err := session.StdinPipe()
+	if err != nil {
+		fmt.Printf("Unable to setup stdin for session: %v", err)
+	}
+	go io.Copy(stdin, os.Stdin)
+
+	stdout, err := session.StdoutPipe()
+	if err != nil {
+		fmt.Printf("Unable to setup stdout for session: %v", err)
+	}
+	go io.Copy(os.Stdout, stdout)
+
+	stderr, err := session.StderrPipe()
+	if err != nil {
+		fmt.Printf("Unable to setup stderr for session: %v", err)
+	}
+	go io.Copy(os.Stderr, stderr)
+
+	err = session.Run("ls -l")
+}
