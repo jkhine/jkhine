cc61d9bd5fb4ea132a59b8ae7d5c45e8a1a51218
Joe Khine <joekhine@gmail.com>
2019-01-04 00:26:39 -0500

    kssh 2

--- kssh/main.go
+++ kssh/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -12,6 +13,116 @@ import (
 
 	"golang.org/x/crypto/ssh"
 )
+
+type SSHCommand struct {
+	Path   string
+	Env    []string
+	Stdin  io.Reader
+	Stdout io.Writer
+	Stderr io.Writer
+}
+
+type SSHClient struct {
+	Config *ssh.ClientConfig
+	Host   string
+	Port   int
+}
+
+func (client *SSHClient) RunCommand(cmd *SSHCommand) error {
+	var (
+		session *ssh.Session
+		err     error
+	)
+
+	if session, err = client.newSession(); err != nil {
+		return err
+	}
+	defer session.Close()
+
+	if err = client.prepareCommand(session, cmd); err != nil {
+		return err
+	}
+
+	err = session.Run(cmd.Path)
+	return err
+}
+
+func (client *SSHClient) prepareCommand(session *ssh.Session, cmd *SSHCommand) error {
+	for _, env := range cmd.Env {
+		variable := strings.Split(env, "=")
+		if len(variable) != 2 {
+			continue
+		}
+
+		if err := session.Setenv(variable[0], variable[1]); err != nil {
+			return err
+		}
+	}
+
+	if cmd.Stdin != nil {
+		stdin, err := session.StdinPipe()
+		if err != nil {
+			return fmt.Errorf("Unable to setup stdin for session: %v", err)
+		}
+		go io.Copy(stdin, cmd.Stdin)
+	}
+
+	if cmd.Stdout != nil {
+		stdout, err := session.StdoutPipe()
+		if err != nil {
+			return fmt.Errorf("Unable to setup stdout for session: %v", err)
+		}
+		go io.Copy(cmd.Stdout, stdout)
+	}
+
+	if cmd.Stderr != nil {
+		stderr, err := session.StderrPipe()
+		if err != nil {
+			return fmt.Errorf("Unable to setup stderr for session: %v", err)
+		}
+		go io.Copy(cmd.Stderr, stderr)
+	}
+
+	return nil
+}
+
+func (client *SSHClient) newSession() (*ssh.Session, error) {
+	connection, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", client.Host, client.Port), client.Config)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to dial: %s", err)
+	}
+
+	session, err := connection.NewSession()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create session: %s", err)
+	}
+
+	modes := ssh.TerminalModes{
+		// ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		session.Close()
+		return nil, fmt.Errorf("request for pseudo terminal failed: %s", err)
+	}
+
+	return session, nil
+}
+
+func PublicKeyFile(file string) ssh.AuthMethod {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		return nil
+	}
+	return ssh.PublicKeys(key)
+}
 
 func getHostKey(host string) (ssh.PublicKey, error) {
 	file, err := os.Open(filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts"))
@@ -57,45 +168,55 @@ func main() {
 		HostKeyCallback: ssh.FixedHostKey(hostKey),
 	}
 
-	connection, err := ssh.Dial("tcp", "192.168.1.55:22", sshConfig)
-	if err != nil {
-		fmt.Printf("Failed to dial: %s", err)
+	client := &SSHClient{
+		Config: sshConfig,
+		Host:   "192.168.1.55",
+		Port:   22,
 	}
 
-	session, err := connection.NewSession()
-	if err != nil {
-		fmt.Printf("Failed to create session: %s", err)
+	cmd := &SSHCommand{
+		Path:   "ls -l $LC_DIR",
+		Env:    []string{"LC_DIR=/"},
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
 	}
 
-	modes := ssh.TerminalModes{
-		ssh.ECHO:          0,     // disable echoing
-		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
-		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	fmt.Printf("Running command: %s\n", cmd.Path)
+	if err := client.RunCommand(cmd); err != nil {
+		fmt.Fprintf(os.Stderr, "command run error: %s\n", err)
+		os.Exit(1)
 	}
 
-	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
-		session.Close()
-		fmt.Printf("request for pseudo terminal failed: %s", err)
-		log.Fatal()
+	cmd = &SSHCommand{
+		Path:   "ip addr",
+		Env:    []string{""},
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
 	}
 
-	stdin, err := session.StdinPipe()
-	if err != nil {
-		fmt.Printf("Unable to setup stdin for session: %v", err)
+	fmt.Printf("Running command: %s\n", cmd.Path)
+	if err := client.RunCommand(cmd); err != nil {
+		fmt.Fprintf(os.Stderr, "command run error: %s\n", err)
+		os.Exit(1)
 	}
-	go io.Copy(stdin, os.Stdin)
 
-	stdout, err := session.StdoutPipe()
-	if err != nil {
-		fmt.Printf("Unable to setup stdout for session: %v", err)
+	cmd = &SSHCommand{
+		Path:   "hostnamectl",
+		Env:    []string{""},
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
 	}
-	go io.Copy(os.Stdout, stdout)
 
-	stderr, err := session.StderrPipe()
-	if err != nil {
-		fmt.Printf("Unable to setup stderr for session: %v", err)
+	fmt.Printf("Running command: %s\n", cmd.Path)
+	if err := client.RunCommand(cmd); err != nil {
+		fmt.Fprintf(os.Stderr, "command run error: %s\n", err)
+		os.Exit(1)
 	}
-	go io.Copy(os.Stderr, stderr)
 
-	err = session.Run("ls -l")
+	for {
+
+	}
 }
