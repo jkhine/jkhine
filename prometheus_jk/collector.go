8c6e5b6dae599cd6230034b7d976ecc3927d84d7
Joe Khine <joekhine@gmail.com>
2019-11-03 18:26:29 -0500

    prometheus client timed

--- prometheus_jk/collector.go
+++ prometheus_jk/collector.go
@@ -1,17 +1,29 @@
 package main
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"time"
+)
 
 var fooMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "foo_metric", Help: "foo help"}, []string {"hostname", "function"},)
 var barMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "bar_metric", Help: "bar help"}, []string {"hostname", "function"},)
+
+func updateMetrics() {
+	go func() {
+		for {
+			fooMetric.WithLabelValues("ccsw1.sqx.colo", "cc").Add(10)
+			barMetric.WithLabelValues("ocsw1.dft.colo", "oc").Add(5)
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
 
 func init() {
 	prometheus.MustRegister(fooMetric)
 	prometheus.MustRegister(barMetric)
 
-	fooMetric.WithLabelValues("ccsw1.sqx.colo", "cc").Add(10)
-	barMetric.WithLabelValues("ocsw1.dft.colo", "oc").Add(5)
-
+	updateMetrics()
 }
 
 
